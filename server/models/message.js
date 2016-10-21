342788c939644ae4ef871d53f926c1203db7bdac
HONG ZHOU <holly_zhou@yahoo.com>
2016-10-21 00:25:28 -0700

    finish backend

--- server/models/message.js
+++ server/models/message.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
+  useName: { type: String, require: true },
   content: { type: String, require: true },
   createAt: { type: Date, default: Date.now, required: true },
 });
